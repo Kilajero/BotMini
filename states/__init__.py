0c7323a60f0ad7dbbf831f52785d3eea9d2d7b6f
Kilajero <kilajero1111s@proton.me>
2025-02-16 09:29:56 -0500

    Delete states directory

--- states/__init__.py
+++
@@ -1,5 +0,0 @@
-from .state_functions import *
-from .state_settings import *
-from .state_payment import *
-from .state_users import *
-from .state_items import *
--- states/state_settings.py
+++
@@ -1,8 +0,0 @@
-# - *- coding: utf- 8 - *-
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class StorageSettings(StatesGroup):
-    here_contact = State()
-    here_faq = State()
-
